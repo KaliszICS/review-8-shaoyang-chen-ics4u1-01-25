f1193d61829c60dee524fed819677538aca433ce
Shaoyang Chen <shaoyang.chen@student.tdsb.on.ca>
2025-09-16 18:48:09 +0000

    Submission V1.2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -26,15 +26,15 @@ public class PracticeProblem {
         if (year % 4 == 0) {
             if (year % 100 == 0) {
                 if (year % 400 == 0) {
-                    return false;
-                } else {
                     return true;
+                } else {
+                    return false;
                 }
             } else {
-                return true;
+                return false;
             }
         } else {
-            return false;
+            return true;
         }
     }
 }
